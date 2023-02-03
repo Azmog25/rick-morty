180c1f6dd339ab4adb6ded63f2d08b190124b549
Azmog25 <Xxhandball25xX@gmail.com>
2023-02-03 09:25:36 +0100

    suppression du fichier CharCarousel.js

--- src/components/CharCarousel.js
+++
@@ -1,9 +0,0 @@
-import React, {useEffect, useState} from "react";
-import {Cookies} from 'react-cookie'
-import Personnage from "./personnages/Personnage";
-
-function CharCarousel() {
-
-}
-
-export default CharCarousel
